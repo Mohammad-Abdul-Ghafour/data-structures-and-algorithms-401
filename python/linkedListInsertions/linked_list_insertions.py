4acdca529ea1694bb5229b285e0d68a94a88a40b
Mohammad Abdul Ghafour <mohammad1478971539@gmail.com>
2021-11-10 22:26:03 +0200

    Fixing Code

--- python/linkedListInsertions/linked_list_insertions.py
+++ python/linkedListInsertions/linked_list_insertions.py
@@ -2,7 +2,7 @@ from typing import Counter
 
 
 class Node:
-    def __init__(self,value):
+    def __init__(self,value = None):
         self.value = value
         self.next = None
 
@@ -80,8 +80,34 @@ class LinkedList:
             outPut += "X"
             return outPut
 
+# def zip_lists(linkedList1 , linkedList2):
+#     try:
+#         a = linkedList1.head
+#         b = linkedList2.head
+#         if a is None and b is None:
+#             raise ValueError("The two linked list are empty")
+#         elif a is None:
+#             return linkedList2.__str__()
+#         elif b is None:
+#             return linkedList1.__str__()
+#         else:
+#             while b != None:
+#                 linkedList1.insertAfter(a.value,b.value)
+#                 print(linkedList1)
+#                 if a.next != None:    
+#                     a = a.next
+#                 if a.next != None:    
+#                     a = a.next
+#                 b = b.next
+#         return linkedList1.__str__()
+#     except AttributeError:
+#         return "One or two of the inputs is not a linked list"
+
 def zip_lists(linkedList1 , linkedList2):
     try:
+
+        dummy = Node()
+        tail = dummy
         a = linkedList1.head
         b = linkedList2.head
         if a is None and b is None:
@@ -91,35 +117,64 @@ def zip_lists(linkedList1 , linkedList2):
         elif b is None:
             return linkedList1.__str__()
         else:
-            while b != None:
-                linkedList1.insertAfter(a.value,b.value)
-                print(linkedList1)
-                if a.next != None:    
-                    a = a.next
-                if a.next != None:    
-                    a = a.next
+            while True:
+                if a is None:
+                    tail.next = b
+                    break
+                elif b is None:
+                    tail.next = a
+                    break
+                tail.next = a
+                tail = a
+                a = a.next
+    
+                tail.next = b
+                tail = b
                 b = b.next
         return linkedList1.__str__()
     except AttributeError:
         return "One or two of the inputs is not a linked list"
  
+
+
+def palindrome_linked_list(ll):
+    a = ll.__str__()
+    if ll.head == None:
+        return "The linked list is empty"
+    current = ll.head
+    while current.next != None:
+        temp_node = current.next
+        current.next = current.next.next
+        temp_node.next = ll.head
+        ll.head = temp_node
+    if a == ll.__str__():
+        return True
+    return False
+
+        
 if __name__ == '__main__':
     ll = LinkedList()
     ll2 = LinkedList()
     ll.append(1)
     ll.append(2)
-    ll.append(6)
+    ll.append(2)
+    ll.append(3)
     ll.append(7)
     ll2.append(3)
     ll2.append(4)
     ll2.append(5)
-    print(ll)
+    a = print(ll)
+    # a = ll
     print(ll2)
-    a = 1
-    b = 2
-    print(zip_lists(a,b)) 
+    # a = 1
+    # b = 2
+    print(zip_lists(ll,ll2)) 
+    # l = palindrome_linked_list(ll)
+    # print(l)
+    # l = palindrome_linked_list(ll)
+    # print(l)
+    # b = str(print(ll))
 
 
 
 
-    
